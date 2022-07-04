cdc82ded1230df8825e383505fb45acfb3e36033
오경식 <74063975+oks9924@users.noreply.github.com>
2022-07-04 18:30:17 +0900

    대머리 추가

---
+++ 대머리/4.6.3.cpp
@@ -0,0 +1,29 @@
+/*#include <vector>
+#include <iostream>
+#include "std_lib_facilities.h"
+
+int main() {
+
+	std::vector<int> x = {1,2,3,4,5,6};
+	for (int y : x)
+		std::cout << y<<'\n';
+	
+
+	std::vector<double> temps;
+	for (double temp; std::cin >> temp;)
+	{
+		if (temp == 0)
+			break;
+		temps.push_back(temp);
+	}
+	
+	vector<double> temps;
+	for (double temp; std::cin >> temp;)
+		temps.push_back(temp);
+	int sum = 0;
+	for (int x : temps)
+		sum += x;
+	std::cout << "¿Âµµ Æò±Õ: "<<sum / temps.size() << '\n';
+	sort(temps);
+	cout << "¿Âµµ Áß¾Ó°ª: " << temps[temps.size() / 2]<<'\n';
+}*/
---
+++ 대머리/4.6.4.cpp
@@ -0,0 +1,21 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	vector<string> words;
+	vector<string> fun;
+	for (string temp; cin >> temp;)
+		words.push_back(temp);
+	cout << "단어 개수: " << words.size()<<'\n';
+
+	sort(words);
+
+	for (int i = 0; i < words.size(); i++)
+	{
+		if(i==0||words[i-1]!=words[i])
+			fun.push_back(words[i]);
+	}
+
+	for (string x : fun)
+		cout << x << '\n';
+
+}
