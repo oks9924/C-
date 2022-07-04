384a234fcc34c88080ab665ca115a2e3bf3ade47
오경식 <74063975+oks9924@users.noreply.github.com>
2022-07-04 18:41:52 +0900

    push

--- 대머리/4.6.3.cpp
+++ 대머리/4.6.3.cpp
@@ -1,19 +1,16 @@
-<<<<<<< Updated upstream
+
 #include <vector>
-=======
-﻿#include <vector>
->>>>>>> Stashed changes
 #include <iostream>
 #include "std_lib_facilities.h"
 
 int main() {
 
-	std::vector<int> x = {1,2,3,4,5,6};
+	vector<int> x = {1,2,3,4,5,6};
 	for (int y : x)
 		std::cout << y<<'\n';
 	
 
-	std::vector<double> temps;
+	vector<double> temps;
 	for (double temp; std::cin >> temp;)
 	{
 		if (temp == 0)
@@ -27,14 +24,9 @@ int main() {
 	int sum = 0;
 	for (int x : temps)
 		sum += x;
-<<<<<<< Updated upstream
-	std::cout << "¿Âµµ Æò±Õ: "<<sum / temps.size() << '\n';
-	sort(temps);
-	cout << "¿Âµµ Áß¾Ó°ª: " << temps[temps.size() / 2]<<'\n';
-}
-=======
+
 	std::cout << "온도 평균: "<<sum / temps.size() << '\n';
 	sort(temps);
 	cout << "온도 중앙값: " << temps[temps.size() / 2]<<'\n';
 }
->>>>>>> Stashed changes
+
